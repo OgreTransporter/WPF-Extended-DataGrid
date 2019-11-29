b700444590e78483ca8a30f69fc4bef04d79a8a5
Transporter <ogre.transporter@gmail.com>
2019-11-29 17:24:45 +0100

    Initial commit with bugs

---
+++ AdvancedDataGrid/UserControls/XMLViewer.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Xml;
+
+namespace TestClient.UserControls
+{
+    /// <summary>
+    /// Interaction logic for XMLViewer.xaml
+    /// </summary>
+    public partial class XMLViewer : UserControl
+    {
+        public XMLViewer()
+        {
+            InitializeComponent();
+        }
+
+        private XmlDocument _xmldocument = null;
+        public XmlDocument xmlDocument
+        {
+            get { return this._xmldocument; }
+            set
+            {
+                this._xmldocument = value;
+                if (this._xmldocument == null)
+                {
+                    this.xmlTree.ItemsSource = null;
+                    return;
+                }
+
+                XmlDataProvider provider = new XmlDataProvider();
+                provider.Document = this._xmldocument;
+                Binding binding = new Binding();
+                binding.Source = provider;
+                binding.XPath = "child::node()";
+                this.xmlTree.SetBinding(TreeView.ItemsSourceProperty, binding);
+            }
+        }
+    }
+}
